fb4e34cb3e6387013062743e88e54c5482389269
polatpoyraz-1 <polat64polat34@gmail.com>
2020-11-11 20:22:13 +0300

    server listen port upadate (8000)

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use('/src', express.static(path.join(__dirname, 'src')))
+app.use('/managment', express.static(path.join(__dirname, 'managment')))
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
-}).listen(1000)
+}).listen(8000)
