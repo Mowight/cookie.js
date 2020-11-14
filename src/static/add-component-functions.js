f65c29a4d74be6ca1f666254910ad466536ed4cd
polatpoyraz-1 <polat64polat34@gmail.com>
2020-11-14 16:18:26 +0300

    add-component-functions bug fixed

--- src/static/add-component-functions.js
+++ src/static/add-component-functions.js
@@ -2,8 +2,6 @@ export const addComponentFunctions = (functions) => {
     for (const prop in functions) {
         if (componentFunctions[prop] === undefined) {
             componentFunctions[prop] = functions[prop]
-        } else {
-            console.error(`function named ${prop} already exists`)
         }
     }
 }
