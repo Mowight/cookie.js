4d1a5be5cf5c93beaf7d65d2a37febb49b0c45c6
polatpoyraz-1 <polat64polat34@gmail.com>
2020-11-17 02:09:13 +0300

    an excess was deleted

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,14 +1,7 @@
 import { propTypesControl, typesConsumer, NUMBER } from '../static/prop-type-control.js'
-import { addComponentFunctions } from '../static/add-component-functions.js'
 
 export const NavLink = (props) => {
     const types = typesConsumer()
-
-    addComponentFunctions({
-        test: () => {
-            alert()
-        }
-    })
 
     propTypesControl(props).controls({
         path: types.String(true),
